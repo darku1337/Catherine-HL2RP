3fcca6a868dcd50df9d161906b38ae5101bd731f
fayzgolden666 <austinwoods59@gmail.com>
2015-03-12 21:22:55 -0600

    fixed english

--- gamemode/schema/items/bag/sh_large_bag.lua
+++ gamemode/schema/items/bag/sh_large_bag.lua
@@ -1,6 +1,6 @@
 Item.base = "bag_base"
 Item.name = "Large bag"
-Item.desc = "This item using add inventory space!"
+Item.desc = "This item adds more inventory space!"
 Item.cost = 300
 Item.model = "models/props_junk/cardboard_box004a_gib01.mdl"
 Item.weight = 1
